8eb865ecb0301429b99d9598553b6462a6dc9a4b
Alex Perry <alexperry2@gmail.com>
2015-09-19 15:51:19 +0100

    Added global scss

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,5 +1,6 @@
 var React = require('react');
 require('bootstrap-sass/assets/stylesheets/_bootstrap.scss');
+require('./components/global.scss');
 var App = React.createFactory(require("./components/app"));
 
 React.render(App(), document.getElementById("app-container"));

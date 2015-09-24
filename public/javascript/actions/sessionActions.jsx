ec7b2c968f414dca167205030a2a48556a93932e
Alex Perry <alexperry2@gmail.com>
2015-09-24 07:47:16 +0100

    Added get user request to session actions

--- public/javascript/actions/sessionActions.jsx
+++ public/javascript/actions/sessionActions.jsx
@@ -1,20 +1,38 @@
 var alt = require('../alt');
 var request = require('superagent');
 
-var actions = {
+var sessionActions = {
     authenticate(data) {
         request
         .post('http://generation.com:3000/authenticate')
         .send(data)
         .end((err,res) => {
+            if(err){
+                return this.actions.authenticationErrorResponse(err)
+            }
             this.actions.authenticateResponse(res);
         });
         this.dispatch();
     },
     authenticateResponse(authenticationResponse) {
         this.dispatch(authenticationResponse);
+        this.actions.getUser(authenticationResponse.body.token)
     },
-
+    authenticationErrorResponse(err) {
+        this.dispatch(err)
+    },
+    getUser(authenticationToken) {
+        request
+        .get('http://generation.com:3000/user')
+        .set('x-access-token', authenticationToken)
+        .end((err,res) => {
+            this.actions.getUserResponse(res);
+        });
+        this.dispatch();
+    },
+    getUserResponse(getUserResponse) {
+        this.dispatch(getUserResponse);
+    }
 };
 
-module.exports = alt.createActions(actions);
+module.exports = alt.createActions(sessionActions);

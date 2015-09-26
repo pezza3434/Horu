3ceae3850a8f57f36e3fae20e4e12703d4fd3aec
Alex Perry <alexperry2@gmail.com>
2015-09-26 16:59:10 +0100

    Switched isotope store to use class

--- public/javascript/stores/isotopeStore.jsx
+++ public/javascript/stores/isotopeStore.jsx
@@ -1,32 +1,35 @@
 var alt = require('../alt');
 var isotopeActions = require('../actions/isotopeActions');
 
-var isotopeStore = function () {
+class isotopeStore {
+    constructor() {
+        this.isotopeImages = [];
+        this.apiCallInProgress = false;
 
-    this.isotopeImages = [];
-    this.apiCallInProgress = false;
+        this.bindListeners({
+            getImages: isotopeActions.getImages,
+            getImagesSuccess: isotopeActions.getImagesSuccess,
+        });
+    }
 
-    this.bindListeners({
-        getImages: isotopeActions.getImages,
-        getImagesSuccess: isotopeActions.getImagesSuccess,
-    });
+    getImages () {
+        this.apiCallInProgress = true;
+    }
+
+    getImagesSuccess (response) {
+        this.isotopeImages = response.body;
+        this.apiCallInProgress = false;
+    }
+
+    submitAge () {
+        this.apiCallInProgress = true;
+    }
+
+    submitAgeSuccess () {
+        this.apiCallInProgress = false;
+    }
 }
 
-isotopeStore.prototype.getImages = function () {
-    this.apiCallInProgress = true;
-}
 
-isotopeStore.prototype.getImagesSuccess = function (response) {
-    this.isotopeImages = response.body;
-    this.apiCallInProgress = false;
-}
 
-isotopeStore.prototype.submitAge = function () {
-    this.apiCallInProgress = true;
-}
-
-isotopeStore.prototype.submitAgeSuccess = function () {
-    this.apiCallInProgress = false;
-}
-
-module.exports = alt.createStore(isotopeStore, 'TestStore');
+module.exports = alt.createStore(isotopeStore, 'isotopeStore');

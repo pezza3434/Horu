6c5e19b16ebe73ae94d15eb5e39c2e447388f630
Alex Perry <alexperry2@gmail.com>
2015-09-26 17:00:07 +0100

    Changed session store to use class

--- public/javascript/stores/sessionStore.jsx
+++ public/javascript/stores/sessionStore.jsx
@@ -1,53 +1,76 @@
 var alt = require('../alt');
 var sessionActions = require('../actions/sessionActions');
 
-var sessionStore = function () {
-    this.isLoggedIn = false;
-    this.authenticationToken = '';
-    this.user = '';
+class sessionStore {
+    constructor() {
+        this.isLoggedIn = false;
+        this.authenticationToken = '';
+        this.user = '';
 
-    this.on('beforeEach', function () {
-        this.apiCallInProgress = false;
-        this.isError = false;
-    });
+        this.on('beforeEach', function () {
+            this.apiCallInProgress = false;
+            this.isError = false;
+            this.registrationError = false;
+            this.successfulRegistration = false;
+        });
 
-    this.bindListeners({
-        authenticate: sessionActions.authenticate,
-        authenticateResponse: sessionActions.authenticateResponse,
-        authenticationErrorResponse: sessionActions.authenticationErrorResponse,
-        getUser: sessionActions.getUser,
-        getUserResponse: sessionActions.getUserResponse
-    });
+        this.bindListeners({
+            authenticate: sessionActions.authenticate,
+            authenticateResponse: sessionActions.authenticateResponse,
+            authenticationErrorResponse: sessionActions.authenticationErrorResponse,
+            getUser: sessionActions.getUser,
+            getUserResponse: sessionActions.getUserResponse,
+            postUser: sessionActions.postUser,
+            postUserResponse: sessionActions.postUserResponse,
+            postUserError: sessionActions.postUserError
+        });
 
-    this.exportPublicMethods({
-        isLoggedIn: function () {
-            return this.getState().isLoggedIn;
-        },
-        getAuthenticationToken: function () {
-            return this.authenticationToken;
-        }
-    });
-};
+        this.exportPublicMethods({
 
-sessionStore.prototype.authenticate = function () {
-    this.apiCallInProgress = true;
-}
+            isLoggedIn() {
+                return this.getState().isLoggedIn;
+            },
 
-sessionStore.prototype.authenticateResponse = function (response) {
-    this.isLoggedIn = true;
-    this.authenticationToken = response.body.token;
-}
+            getAuthenticationToken() {
+                return this.authenticationToken;
+            }
 
-sessionStore.prototype.authenticationErrorResponse = function (err) {
-    this.isError = true;
-}
+        });
+    }
 
-sessionStore.prototype.getUser = function () {
-    this.apiCallInProgress = true;
-}
+    authenticate () {
+        this.apiCallInProgress = true;
+    }
 
-sessionStore.prototype.getUserResponse = function (response) {
-    this.user = response.body;
+    authenticateResponse (response) {
+        this.isLoggedIn = true;
+        this.authenticationToken = response.body.token;
+    }
+
+    authenticationErrorResponse (err) {
+        this.isError = true;
+    }
+
+    getUser () {
+        this.apiCallInProgress = true;
+    }
+
+    getUserResponse (response) {
+        this.user = response.body;
+    }
+
+    postUser () {
+        this.apiCallInProgress = true;
+    }
+
+    postUserResponse (response) {
+        this.successfulRegistration = true;
+    }
+
+    postUserError (err) {
+        this.registrationError = true;
+    }
+
 }
 
 module.exports = alt.createStore(sessionStore, 'sessionStore');

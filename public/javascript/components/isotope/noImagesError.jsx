5e10ad337ed5ce28af93bcd4035aa170caa5a5e6
Alex Perry <alexperry2@gmail.com>
2015-11-14 12:23:20 +0000

    Added improved no images error when there's no images available

--- public/javascript/components/isotope/noImagesError.jsx
+++ public/javascript/components/isotope/noImagesError.jsx
@@ -4,7 +4,10 @@ export default React.createClass({
     render() {
         return (
             <div className="col-sm-10 col-sm-offset-2 content no-padding">
-            <div>There are no images to rate!!</div>
+                <div className="no-images-message">
+                    <h3><i className="fa fa-file-image-o" data-reactid=".13ume45kydc.0.1.0.3.0.0.0"></i></h3>
+                    <h3>It looks like you've already rated all the images currently uploaded.</h3>
+                </div>
             </div>
         );
     }

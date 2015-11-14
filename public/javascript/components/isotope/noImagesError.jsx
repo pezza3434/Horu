a76dc613cfdff5fe26979d994185337aaf891b92
Alex Perry <alexperry2@gmail.com>
2015-11-14 17:23:52 +0000

    Updated wrong no-images classes

--- public/javascript/components/isotope/noImagesError.jsx
+++ public/javascript/components/isotope/noImagesError.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default React.createClass({
     render() {
         return (
-            <div className="col-sm-10 col-sm-offset-2 content no-padding">
+            <div className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 content no-padding">
                 <div className="no-images-message">
                     <h3><i className="fa fa-file-image-o" data-reactid=".13ume45kydc.0.1.0.3.0.0.0"></i></h3>
                     <h3>It looks like you've already rated all the images currently uploaded.</h3>

7fa446d2c925f98f8aab7c1a6600a3363c96dce7
Alex Perry <alexperry2@gmail.com>
2015-11-20 23:32:20 +0000

    Added shirt deployment mechanism

---
+++ shipitfile.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: '/tmp/github-monitor',
+      deployTo: '/tmp/deploy_to',
+      repositoryUrl: 'https://github.com/pezza3434/Horu',
+      ignores: ['.git', 'node_modules'],
+      keepReleases: 2,
+      deleteOnRollback: false,
+      shallowClone: true
+    },
+    production: {
+      servers: 'root@horu.io'
+    }
+  });
+  shipit.on('updated', function () {
+    var buildDirectory = path.resolve('./public/');
+    shipit.remoteCopy(buildDirectory, shipit.releasePath);
+  });
+};

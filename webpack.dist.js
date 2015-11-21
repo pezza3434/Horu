66664692ab9bbeef465e3e290fba597de1e1a7a4
Alex Perry <alexperry2@gmail.com>
2015-11-21 17:30:51 +0000

    Added minifaction to the bundle

---
+++ webpack.dist.js
@@ -0,0 +1,42 @@
+var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+
+module.exports = [{
+    entry: "./public/javascript/app.js",
+    output: {
+        filename: "./public/javascript/bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader'
+        },
+        {
+            test: /\.jsx?$/,
+            exclude: /(node_modules)/,
+            loader: 'babel'
+        },{
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }, {
+            test: /\.(ttf|eot|svg|woff2|woff)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "file-loader"
+        }, {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract(
+                // activate source maps via loader query
+                'css?sourceMap!' +
+                'sass?sourceMap&')
+        }, ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    plugins: [
+        new ExtractTextPlugin("./public/styles.css"),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.DedupePlugin()
+    ]
+}];

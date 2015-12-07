1d979dafea2f7c36319653d9c6e79f2fe71af79c
Alex Perry <alexperry2@gmail.com>
2015-12-07 08:01:47 +0000

    Added social share for when user has no votes

--- public/javascript/components/account/ratings/individual-rating/index.jsx
+++ public/javascript/components/account/ratings/individual-rating/index.jsx
@@ -11,12 +11,24 @@ export default React.createClass({
         twttr.widgets.load();
     },
     shareFb() {
+
+        if (this.props.rating.votes) {
+            return FB.ui({
+                method: 'feed',
+                link: 'http://horu.io',
+                description: 'My picture has been rated by hundreds of people and I look ' + Math.floor(this.props.rating.current_rating) + '. Horu is the online platform for finding out how old you look. Get started today!',
+                caption: 'http://horu.io',
+                name: 'I look ' + Math.floor(this.props.rating.current_rating) + '!',
+                picture: this.props.serverUrl + '/static' + this.props.rating.image_thumbnail
+            }, function(){});
+        }
+
         FB.ui({
             method: 'feed',
             link: 'http://horu.io',
-            description: 'My picture has been rated by hundreds of people and I look ' + Math.floor(this.props.rating.current_rating) + '. Horu is the online platform for finding out how old you look. Get started today!',
+            description: 'Guess how old I am on Horu!',
             caption: 'http://horu.io',
-            name: 'I look ' + Math.floor(this.props.rating.current_rating) + '!',
+            name: 'Guess how old I look',
             picture: this.props.serverUrl + '/static' + this.props.rating.image_thumbnail
         }, function(){});
     },
@@ -37,6 +49,9 @@ export default React.createClass({
                         <strong>Number of Votes: </strong>
                         {this.props.rating.votes}
                     </div>
+                    <div>
+                        Share your age:
+                    </div>
                     <div className="social-icons">
                         <div className="twitter"><a className="twitter-share-button-custom" href={'https://twitter.com/intent/tweet?text=I%20look%20' + Math.floor(this.props.rating.current_rating) + '%21.%20How%20old%20do%20you%20look%3F&url=http://horu.io&via=thehoruapp'}><i className="fa fa-twitter"></i></a></div>
                         <div className="facebook"><a href="#" onClick={this.shareFb}><i className="fa fa-facebook"></i></a></div>
@@ -48,9 +63,12 @@ export default React.createClass({
                 </div>
                 :
                 <div className="col-xs-8 account__uploads__message">
-                    <div><strong>Check back soon...</strong></div>
-                    <div>People are guessing your age.</div>
-
+                    <div><strong>People are guessing your age.</strong></div>
+                    <div className="account__uploads__message__invite">Feeling impatient? Invite your friends to guess your age:</div>
+                    <div className="social-icons">
+                        <div className="twitter"><a className="twitter-share-button-custom" href={'https://twitter.com/intent/tweet?text=Guess%20how%20old%20I%20look%20on%20Horu%21&url=http://horu.io&via=thehoruapp'}><i className="fa fa-twitter"></i></a></div>
+                        <div className="facebook"><a href="#" onClick={this.shareFb}><i className="fa fa-facebook"></i></a></div>
+                    </div>
                     <DeleteImage
                         deleteAction={this.props.triggerDeleteModal}
                         imageId={this.props.rating.image_id}

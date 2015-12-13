357ead49c733abcea98180184d63e8562f1a4ed2
Alex Perry <alexperry2@gmail.com>
2015-12-13 10:47:57 +0000

    Updated text for first time viewers

--- public/javascript/components/welcome-message/index.jsx
+++ public/javascript/components/welcome-message/index.jsx
@@ -15,10 +15,8 @@ export default React.createClass({
         return (
             <div className={cn('col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 content welcome-message', {closed: !this.props.open})}>
                 <span className="welcome-message__icon"><i onClick={this._clickClose} className="fa fa-times"></i></span>
-                <h1>Hey there! Welcome to Horu.</h1>
-                <p>Horu is the platform for finding out exactly how old you look. Simply upload one or multiple photos of yourself and others
-                   can vote on how old they think you look. You then have access to an average of these votes to find out how old you truly look.
-               </p>
+                <h1>Find out exactly how old you look.</h1>
+                <p>Simply upload one or multiple photos of yourself and others can vote on how old they think you look.</p>
                <p className="feeling-brave">Feeling Brave?</p>
                  <RegisterButton buttonClassName="btn btn-lg btn-default" buttonText="Get Started"/>
             </div>
